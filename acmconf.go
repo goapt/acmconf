e930a87e396cb7d2d5970aca16c146d612d6dc6c
fifsky <fifsky@gmail.com>
2018-07-06 14:45:11 +0800

    init commit

---
+++ acmconf.go
@@ -0,0 +1,106 @@
+package acmconf
+
+import (
+	"github.com/verystar/goacm"
+	"reflect"
+	"errors"
+	"github.com/ilibs/json5"
+	"strings"
+	"sync"
+	"time"
+)
+
+type acmItem struct {
+	dataId string
+	group  string
+	v      interface{}
+}
+
+type Config struct {
+	Client *goacm.Client
+	Tag    string
+	cache  sync.Map
+}
+
+func NewConfig(option func(c *goacm.Client)) (*Config, error) {
+	client, err := goacm.NewClient(option)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		Client: client,
+		Tag:    "acmconf",
+		cache:  sync.Map{},
+	}, nil
+}
+
+func (c *Config) Get(dataId, group string) (string, error) {
+	return c.Client.GetConfig(dataId, group)
+}
+
+//Unmarshal is json5 unmarshal to struct and support xpath
+func (c *Config) Unmarshal(dataId, group string, v interface{}) error {
+	result, err := c.Get(dataId, group)
+
+	if err != nil {
+		return err
+	}
+
+	buf := []byte(result)
+	err = json5.Unmarshal(buf, v)
+	if err == nil {
+		c.cache.Store(c.getCacheKey(dataId, group), &acmItem{
+			dataId: dataId,
+			group:  group,
+			v:      v,
+		})
+	}
+	return err
+}
+
+func (c *Config) Listen(dataId, group string, v interface{}, fn func()) {
+	go func(dataId, grpup string, v interface{}) {
+		for {
+			_, err := c.Client.Subscribe(dataId, group, "")
+			if err == nil {
+				c.Unmarshal(dataId, group, v)
+				fn()
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}(dataId, group, v)
+}
+
+func (c *Config) getCacheKey(dataId, group string) string {
+	return strings.Join([]string{c.Client.NameSpace, dataId, group}, "-")
+}
+
+func (c *Config) Load(v interface{}) error {
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr || rv.IsNil() {
+		return errors.New("config:Load(non-pointer)")
+	}
+	val := rv.Elem()
+	t := reflect.TypeOf(v).Elem()
+
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i).Tag.Get(c.Tag)
+		if f == "-" || f == "" {
+			continue
+		}
+		tmp := strings.Split(f, ",")
+
+		if len(tmp) != 2 {
+			continue
+		}
+
+		err := c.Unmarshal(tmp[0], tmp[1], val.Field(i).Addr().Interface())
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ acmconf_test.go
@@ -0,0 +1,82 @@
+package acmconf
+
+import (
+	"log"
+	"github.com/verystar/goacm"
+	"os"
+	"testing"
+	"time"
+)
+
+func getConfig() *Config {
+	conf, err := NewConfig(func(c *goacm.Client) {
+		c.AccessKey = os.Getenv("AccessKey")
+		c.SecretKey = os.Getenv("SecretKey")
+		c.EndPoint = "acm.aliyun.com"
+		c.NameSpace = os.Getenv("NameSpace")
+		c.TimeOut = 10
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return conf
+}
+
+type App struct {
+	Test map[string]string `acmconf:"test,test"`
+}
+
+var beforJsonConf = `
+{
+  "name": "lili",
+  "mail": "test@test.com",
+  "hello": "world",
+}
+`
+
+var afterJsonConf = `
+{
+  "name": "didi",
+  "mail": "tmp@test.com",
+  "hello": "verystar",
+}
+`
+
+func TestConfig_Listen(t *testing.T) {
+	conf := getConfig()
+	_, err := conf.Client.Publish("test", "test", beforJsonConf)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	//Sleep 3 secend, Ensure configuration effective
+	time.Sleep(3 * time.Second)
+
+	app := &App{}
+	err = conf.Load(app)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	conf.Listen("test", "test", &app.Test, func() {
+		if app.Test["name"] != "didi" {
+			t.Error("app not update")
+		}
+	})
+
+	time.Sleep(5 * time.Second)
+	_, err = conf.Client.Publish("test", "test", afterJsonConf)
+
+	if err != nil {
+		t.Error(err)
+	}
+	time.Sleep(5 * time.Second)
+
+	if app.Test["name"] != "didi" {
+		t.Error("app not update")
+	}
+}
